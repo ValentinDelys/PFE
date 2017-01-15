2961eca10257c7bbbfcd4fea6afeb4f3f88c0ac6
VDelys <valentin.delys@gmail.com>
2017-01-15 21:35:06 +0100

    Added PreProcessing class

---
+++ preprocessing.cpp
@@ -0,0 +1,6 @@
+#include "preprocessing.h"
+
+PreProcessing::PreProcessing()
+{
+
+}
---
+++ preprocessing.h
@@ -0,0 +1,11 @@
+#ifndef PREPROCESSING_H
+#define PREPROCESSING_H
+
+
+class PreProcessing
+{
+public:
+    PreProcessing();
+};
+
+#endif // PREPROCESSING_H
